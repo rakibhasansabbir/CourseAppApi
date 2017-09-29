f2d743ff7d191dfe7b8f1102ec6faab5b2262ad2
rakib <rakibhasansabbir@yahoo.com>
2017-09-29 10:02:30 +0600

    create hello world spring boot application

---
+++ src/main/java/io/javabrains/springbootstarter/CourseAppApp.java
@@ -0,0 +1,14 @@
+package io.javabrains.springbootstarter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseAppApp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CourseAppApp.class, args);
+
+	}
+
+}

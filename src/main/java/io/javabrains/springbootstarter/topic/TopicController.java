93840f1db157dbbe129c8b156332472d0e0789ad
rakib <rakibhasansabbir@yahoo.com>
2017-09-30 18:02:54 +0600

    Implementing update and delete

--- src/main/java/io/javabrains/springbootstarter/topic/TopicController.java
+++ src/main/java/io/javabrains/springbootstarter/topic/TopicController.java
@@ -25,6 +25,15 @@ public class TopicController {
     @RequestMapping(method = RequestMethod.POST, value = "/topic")
     public void addTopic(@RequestBody Topic topic){
         topicService.addTopic(topic);
+    }
 
+    @RequestMapping(method = RequestMethod.PUT, value = "/topic/{id}")
+    public void updateTopic(@RequestBody Topic topic,@PathVariable String id){
+        topicService.updateTopic(id,topic);
+    }
+
+    @RequestMapping(method = RequestMethod.DELETE, value = "/topic/{id}")
+    public void deleteTopic(@PathVariable String id){
+        topicService.deleteTopic(id);
     }
 }
